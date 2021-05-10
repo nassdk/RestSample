a8f10bb1b75f0b00dda8dcc04c7a2dfcf0d2d8d0
nassdk <nassdk17@gmail.com>
2021-05-11 02:03:46 +0300

    Finished starter screen.

--- app/src/main/java/com/example/rest/feature/starter/StarterScreen.kt
+++ app/src/main/java/com/example/rest/feature/starter/StarterScreen.kt
@@ -1,31 +1,36 @@
 package com.example.rest.feature.starter
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.rest.R
 
 @Preview
 @Composable
 fun StarterScreen() {
 
-    LazyColumn(
+    Column(
         content = {
-            item { Toolbar() }
-            item { ButtonsRow() }
+            Toolbar()
+            ButtonsColumn()
         },
         modifier = Modifier
             .background(color = MaterialTheme.colors.primary)
@@ -38,11 +43,29 @@ fun Toolbar() {
 
     TopAppBar(
         title = {
-            Text(
-                text = "VERANDA",
-                color = MaterialTheme.colors.secondary,
-                modifier = Modifier.fillMaxWidth(fraction = 1f),
-                textAlign = TextAlign.Center
+
+            Box(
+                content = {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_logo),
+                        contentDescription = "LogoIcon",
+                        modifier = Modifier
+                            .height(height = 30.dp)
+                            .width(width = 100.dp)
+                            .align(alignment = Alignment.Center)
+                    )
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.align(alignment = Alignment.TopEnd)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_notifications_bell),
+                            contentDescription = "BellIcon"
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxSize()
             )
         },
         modifier = Modifier
@@ -53,15 +76,35 @@ fun Toolbar() {
 }
 
 @Composable
-fun ButtonsRow() {
+fun ButtonsColumn() {
 
-    Column {
-        MenuButton()
-    }
+    Column(
+        content = {
+            MakeDeliveryButton()
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(16.dp)
+            )
+
+            FeedbackButton()
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(16.dp)
+            )
+
+            MakeBookingButton()
+        },
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    )
 }
 
 @Composable
-fun MenuButton() {
+fun MakeDeliveryButton() {
 
     Button(
         onClick = {
@@ -70,16 +113,117 @@ fun MenuButton() {
         modifier = Modifier
             .background(color = MaterialTheme.colors.primary)
             .height(height = 56.dp)
+            .padding(horizontal = 16.dp)
             .fillMaxWidth(),
         border = BorderStroke(
             width = 1.dp,
             color = MaterialTheme.colors.primaryVariant,
-        )
+        ),
+        shape = RoundedCornerShape(size = 4.dp)
     ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_menu),
+            contentDescription = "MenuIcon"
+        )
+
         Text(
-            text = "Просмотреть меню",
+            text = "Оформить доставку",
             fontSize = 16.sp,
-            color = MaterialTheme.colors.secondary
+            color = MaterialTheme.colors.secondary,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .weight(weight = 1f)
+                .padding(start = 8.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_chevron_right_black),
+            contentDescription = "Chevron"
+        )
+    }
+}
+
+@Composable
+fun FeedbackButton() {
+
+    Button(
+        onClick = {
+
+        },
+        modifier = Modifier
+            .background(color = MaterialTheme.colors.primary)
+            .height(height = 56.dp)
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth(),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colors.primaryVariant,
+        ),
+        shape = RoundedCornerShape(size = 4.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_reviews),
+            contentDescription = "ReviewsIcon"
+        )
+
+        Text(
+            text = "Жалобная книга",
+            fontSize = 16.sp,
+            color = MaterialTheme.colors.secondary,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .weight(weight = 1f)
+                .padding(start = 8.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_chevron_right_black),
+            contentDescription = "Chevron"
+        )
+    }
+}
+
+@Composable
+fun MakeBookingButton() {
+
+    Button(
+        onClick = {
+
+        },
+        modifier = Modifier
+            .indication(
+                indication = rememberRipple(bounded = true),
+                interactionSource = remember { MutableInteractionSource() })
+            .background(color = MaterialTheme.colors.primary)
+            .height(height = 56.dp)
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth(),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colors.primaryVariant,
+        ),
+        shape = RoundedCornerShape(size = 4.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_booking),
+            contentDescription = "BookingIcon"
+        )
+
+        Text(
+            text = "Забронировать столик",
+            fontSize = 16.sp,
+            color = MaterialTheme.colors.secondary,
+            textAlign = TextAlign.Start,
+            modifier = Modifier
+                .weight(weight = 1f)
+                .padding(start = 8.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_chevron_right_black),
+            contentDescription = "Chevron"
         )
     }
 }
