425a596099d4fc0e646509de063d7f074abbf9fa
Sadyk Nasrullaev <nassdk17@gmail.com>
2021-08-21 14:24:23 +0300

    Compose dependencies update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    compileSdk = 30
+    compileSdk = 31
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
@@ -41,23 +41,23 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-rc02"
+        kotlinCompilerExtensionVersion = "1.1.0-alpha02"
     }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
 
-    implementation("androidx.compose.ui:ui:1.0.0-rc02")
-    implementation("androidx.compose.material:material:1.0.0-rc02")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-rc02")
+    implementation("androidx.compose.ui:ui:1.1.0-alpha02")
+    implementation("androidx.compose.material:material:1.1.0-alpha02")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0-alpha02")
 
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha08")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-beta02")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-rc02")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
+    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha07")
 
     implementation("com.google.android.material:material:1.4.0")
     implementation("com.google.accompanist:accompanist-coil:0.14.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("com.android.tools.build:gradle:7.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 }
 
